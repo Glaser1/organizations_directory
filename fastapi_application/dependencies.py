fe04788e75c68f36dd709bf35e8698f019df0a6d
Alexandr <sashapliskov@gmail.com>
2025-02-25 16:44:14 +0300

    remove unused import

--- fastapi_application/dependencies.py
+++ fastapi_application/dependencies.py
@@ -1,9 +1,9 @@
-from fastapi import Header, HTTPException, status, Security
+from fastapi import HTTPException, status, Security
 from fastapi.security import APIKeyHeader
 
 from config import settings
 
-api_key_header = APIKeyHeader(name=settings.API_KEY, auto_error=False)
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
 
 
 async def validate_api_key(api_key: str = Security(api_key_header)) -> str:
